23c13b7b311678ed70e14b9ee8755034d988a288
katemic <bigideahuang@foxmail.com>
2021-07-06 14:30:17 +0800

    feat: 修改投诉页主界面

--- miniprogram/pages/complaint/complaint.js
+++ miniprogram/pages/complaint/complaint.js
@@ -5,7 +5,35 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        imageURL: '../../../../pages/complaint/pic.webp',
+        option1: [{
+                text: '全部商品',
+                value: 0
+            },
+            {
+                text: '新款商品',
+                value: 1
+            },
+            {
+                text: '活动商品',
+                value: 2
+            },
+        ],
+        option2: [{
+                text: '默认排序',
+                value: 'a'
+            },
+            {
+                text: '好评排序',
+                value: 'b'
+            },
+            {
+                text: '销量排序',
+                value: 'c'
+            },
+        ],
+        value1: 0,
+        value2: 'a',
     },
 
     /**
@@ -63,12 +91,12 @@ Page({
     onShareAppMessage: function () {
 
     },
-    onGotoForm(){
+    onGotoForm() {
         wx.navigateTo({
-          url: './form/form',
+            url: './form/form',
         })
     },
-    onGotoDetail(){
+    onGotoDetail() {
         wx.navigateTo({
             url: './detail/detail',
         })
