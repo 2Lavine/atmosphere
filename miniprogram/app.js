adc7e825715d2b63b71729966c50c63dfdd10dee
ssr <evelyn_sue@sina.com>
2021-07-07 09:34:13 +0800

    我的页面部分

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,4 +1,20 @@
 //app.js
+const pollutetype = [ //污染类别
+  '黑烟柴油',
+  '油烟排放',
+  '尘土飞扬',
+  '油气泄露',
+  '烟尘排放',
+  '其他'
+]
+
+const state = [ // 投诉状态
+  '已提交',
+  '已受理',
+  '处理中',
+  '整改完成'
+]
+
 App({
   onLaunch: function () {
     var that = this
@@ -14,11 +30,11 @@ App({
   
   globalData: {
     authSetting: null,
-
     openId: null,
     isUserLogin: false,
     userInfo: null,
     userLocation: null,
-
+    polluteType: pollutetype,
+    state: state
   }
 })
